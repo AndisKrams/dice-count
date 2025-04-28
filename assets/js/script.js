ae445e5f12bf0c9bd8e9a9d66af8b2d2af3c7022
AndisKrams <523ku12i@students.codeinstitute.net>
2025-04-28 13:10:20 +0100

    Add game functions

---
+++ assets/js/script.js
@@ -0,0 +1,24 @@
+
+function roll() {
+    const dice1 = Math.floor(Math.random() * 6) + 1;
+    const dice2 = Math.floor(Math.random() * 6) + 1;
+    const dice3 = Math.floor(Math.random() * 6) + 1;
+    const dice4 = Math.floor(Math.random() * 6) + 1;
+    const dice5 = Math.floor(Math.random() * 6) + 1;
+    const dice6 = Math.floor(Math.random() * 6) + 1;
+};
+function displayDice() {
+
+};    
+function sum() {
+
+};
+function checkAnswer() {
+
+};
+function displayResult() {
+
+};
+function timer() {
+
+};    

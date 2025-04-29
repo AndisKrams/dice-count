3228b4683ffa2eaa5684a61d29a057a721c0e8f0
AndisKrams <523ku12i@students.codeinstitute.net>
2025-04-29 13:53:46 +0100

    Add event listeners

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,16 @@
+$( document ).ready(function() {
+    $("#roll").click(function() {
 
-function roll() {
     const dice1 = Math.floor(Math.random() * 6) + 1;
     const dice2 = Math.floor(Math.random() * 6) + 1;
     const dice3 = Math.floor(Math.random() * 6) + 1;
     const dice4 = Math.floor(Math.random() * 6) + 1;
     const dice5 = Math.floor(Math.random() * 6) + 1;
     const dice6 = Math.floor(Math.random() * 6) + 1;
-};
+});
 function displayDice() {
 
-};    
+});
 function sum() {
 
 };

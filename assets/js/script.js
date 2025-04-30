5a88e5d5fc8cf89e85f8bd45f70c665acd9a0749
AndisKrams <523ku12i@students.codeinstitute.net>
2025-04-30 12:38:19 +0100

    Add dice image change functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,13 +7,22 @@ $( document ).ready(function() {
     const dice4 = Math.floor(Math.random() * 6) + 1;
     const dice5 = Math.floor(Math.random() * 6) + 1;
     const dice6 = Math.floor(Math.random() * 6) + 1;
-});
+
+    console.log(dice1, dice2, dice3, dice4, dice5, dice6);
+   
+    $("#dice1").attr("src", "assets/images/dice" + dice1 + ".png");
+    $("#dice2").attr("src", "assets/images/dice" + dice2 + ".png");
+    $("#dice3").attr("src", "assets/images/dice" + dice3 + ".png");
+    $("#dice4").attr("src", "assets/images/dice" + dice4 + ".png");
+    $("#dice5").attr("src", "assets/images/dice" + dice5 + ".png");
+    $("#dice6").attr("src", "assets/images/dice" + dice6 + ".png");
+
+    const sum = dice1 + dice2 + dice3 + dice4 + dice5 + dice6;
+})});
 function displayDice() {
 
-});
-function sum() {
-
 };
+
 function checkAnswer() {
 
 };

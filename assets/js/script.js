104b9b50a31b6408f62379019170ac9a4b76e29f
AndisKrams <523ku12i@students.codeinstitute.net>
2025-05-09 20:17:26 +0100

    Move functions and fix checkAnswer

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,19 @@
-$( document ).ready(function() {
-    $("#roll").click(function() {
+$(document).ready(function () {
+    $("#roll").click(function () {
+        rollDice();
+    });
 
+});
+function rollDice() {
     const dice1 = Math.floor(Math.random() * 6) + 1;
     const dice2 = Math.floor(Math.random() * 6) + 1;
     const dice3 = Math.floor(Math.random() * 6) + 1;
     const dice4 = Math.floor(Math.random() * 6) + 1;
     const dice5 = Math.floor(Math.random() * 6) + 1;
     const dice6 = Math.floor(Math.random() * 6) + 1;
-//test
+    //test
     console.log(dice1, dice2, dice3, dice4, dice5, dice6);
-   
+
     $("#dice1").html('<img src="assets/images/dice' + dice1 + '.png">');
     $("#dice2").html('<img src="assets/images/dice' + dice2 + '.png">');
     $("#dice3").html('<img src="assets/images/dice' + dice3 + '.png">');
@@ -19,35 +23,33 @@ $( document ).ready(function() {
 
     const sum = dice1 + dice2 + dice3 + dice4 + dice5 + dice6;
     const userAnswer = parseInt($("#answer").val());
-    $("#answer").val = "";
-    $("#answer").focus();
 
-    $("#submit-button").click(function() {
-        
+    console.log("Sum: " + sum);
+
+    $("#submit-button").click(function () {
+        checkAnswer();
+    });
+
+    $("#answer").keydown(function (event) {
+        if (event.key === "Enter") {
+            checkAnswer();
+        }
+    });
+
+    function checkAnswer() {
+        const userAnswer = parseInt($("#answer").val());
         if (userAnswer === sum) {
             $("#result-text").html("Correct! The sum is " + sum);
         } else {
             $("#result-text").html("Incorrect! The sum is " + sum);
         }
         console.log("User answer: " + userAnswer);
-    });
+    }
     
-    $("#answer").keydown(function(event) {
-        if (event.key === "enter") {
-            const userAnswer = parseInt($("#answer").val());
-            if (userAnswer === sum) {
-                $("#result-text").html("Correct! The sum is " + sum);
-            } else {
-                $("#result-text").html("Incorrect! The sum is " + sum);
-            }
-            console.log("User answer: " + userAnswer);
-        }
-})});
+    $("#answer").val = "";
+    $("#answer").focus();
+}
 
-});
 
 /*
-function displayResult() {
-
-
 function timer() */
